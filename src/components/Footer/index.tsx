d9505df12d8208587e88bd67160ab3623079b731
Neder <nedersimoes@gmail.com>
2022-02-07 17:43:10 -0400

    custom footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,34 @@
+// components
+import { SiLinkedin, SiGithub } from "react-icons/si";
+
 // styles
 import styles from "./styles.module.scss";
 
 export default function Footer() {
+  function handleRedirect(url: string) {
+    window.open(url);
+  }
+
+  function handleScrollTop() {
+    window.scroll({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+
   return (
     <div className={styles.container}>
-      <h1>Footer</h1>
+      <button onClick={handleScrollTop}>Voltar ao topo</button>
+      <section>
+        <SiLinkedin
+          onClick={() =>
+            handleRedirect("https://www.linkedin.com/in/neder-hayden/")
+          }
+        />
+        <SiGithub
+          onClick={() => handleRedirect("https://github.com/nederhayden")}
+        />
+      </section>
     </div>
   );
 }
