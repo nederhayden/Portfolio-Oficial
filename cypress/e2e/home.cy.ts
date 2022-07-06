eb6aea739c0ac63f0b96da6421cf821a179b0119
Neder <nedersimoes@gmail.com>
2022-07-06 15:58:08 -0400

    add tests cypress

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,10 +1,22 @@
 describe("Tests in my home page", () => {
-  it("visit my home page", () => {
+  it("Visit my home page", () => {
     cy.visit("http://localhost:3000/");
+  });
 
+  it("Visit page PT-BR", () => {
+    cy.visit("http://localhost:3000/pt-BR");
     cy.contains("Sobre").click();
     cy.contains("Conhecimentos").click();
     cy.contains("Contato").click();
+    // cy.contains("Baixar Currículo").click();
+    cy.contains("nederhayden").click();
+  });
+
+  it("Visit page EN-US", () => {
+    cy.visit("http://localhost:3000/en-US");
+    cy.contains("About").click();
+    cy.contains("Acquirements").click();
+    cy.contains("Contact").click();
     // cy.contains("Baixar Currículo").click();
     cy.contains("nederhayden").click();
   });
